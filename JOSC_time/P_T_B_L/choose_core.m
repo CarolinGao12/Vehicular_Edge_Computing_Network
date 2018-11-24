58b70a3ff424b1ce8bd055ea5b7e15381e5e40a0
Wanting Gao <wanting.gao@rwth-aachen.de>
2018-11-24 14:23:24 +0100

    the second situation done

--- JOSC_time/P_T_B_L/choose_core.m
+++ JOSC_time/P_T_B_L/choose_core.m
@@ -1,9 +1,7 @@
-function [result_opti,vehicle_position_delete_right,vehicle_position_delete_left] = add_car_opti(N_right_add,N_left_add,L_m,v_speed,recy_time,V_postion_x_right_initial,V_postion_y_right_initial,V_postion_x_left_initial,V_postion_y_left_initial,Z_L_m,B_RSU,M,P_v,N_v,c_car,F_limit,buffer_number)
-
-
-%   the car add to the direction right
-V_positon_x_right_add= 0.5*ones(N_right_add,1);
-V_positon_y_right_add= rand(N_right_add,1);
+function [result_opti_core,TT,vehicle_position_delete_right,vehicle_position_delete_left,j_selection,vehicle_position_task]=choose_core(F_limit_core,N_right_add,N_left_add,M,L_m,v_speed,B_RSU,P_v,N_v,c_car,recy_time,Z_L_m,Time_delay,V_postion_x_right_initial,V_postion_y_right_initial,V_postion_x_left_initial,V_postion_y_left_initial)
+    M_C=4*M;                      % The number of cores          
+    V_positon_x_right_add= 0.5*ones(N_right_add,1);
+    V_positon_y_right_add= rand(N_right_add,1);
 
 for ii=1: N_right_add
     if V_positon_y_right_add(ii) >=0.5
@@ -30,7 +28,6 @@ end
 V_postion_x_right_initial=V_postion_x_right_initial+v_speed* recy_time;
 V_postion_x_left_initial =V_postion_x_left_initial -v_speed* recy_time;
 
-
 %take out the car not in the range 
 for ii= 1: length(V_postion_x_right_initial)
     if (V_postion_x_right_initial(ii)>= 0) && (V_postion_x_right_initial(ii)<= 6*Z_L_m)
@@ -55,7 +52,6 @@ vehicle_position_delete=vehicle_position_now;
 delete_index=find(vehicle_position_delete(:,1)==0);
 vehicle_position_delete([delete_index],:)=[];
 
-
 %not each car has one task, it has probability to have task 
 task_proba=rand(length(vehicle_position_delete),1);
 vehicle_position_delete=[vehicle_position_delete,task_proba];
@@ -71,8 +67,6 @@ end
 vehicle_position_task = vehicle_position_delete;
 delete_car_index=find(vehicle_position_task(:,3)==0);
 vehicle_position_task([delete_car_index],:)=[];
-
-
 
 % get the number of car right and left
 N_right=0; 
@@ -111,11 +105,15 @@ for ii=1:N_left
      end
 end
 L_all=[L_all_right ; L_all_left];  
+L_all_core=zeros(length(vehicle_position_task),M_C);
+for ii=1:M
+   L_all_core(:,4*ii-3:4*ii)=repmat(L_all(:,ii),1,4); 
+end
 
 
-T_d_all = (zeros(length(vehicle_position_task),M+1));
+T_d_all = (zeros(length(vehicle_position_task),M_C+1));
 for ii = 1:length(vehicle_position_task)
-    T_d_all(ii,2:end) = L_all(ii,:) ./ v_speed;
+    T_d_all(ii,2:end) = L_all_core(ii,:) ./ v_speed;
 end 
 
 %communication time 
@@ -126,106 +124,76 @@ dataSize = (100+200*rand(length(vehicle_position_task),1));
 d_t=sqrt(w_length.^2+h_length.^2);
 r_c= B_RSU* log2(1+(P_v *  (d_t.^(-2.5))  )/N_v);
 T_c= dataSize./r_c;
-
-T_c_all = zeros(length(vehicle_position_task),M+1);
-for ii = 2:M+1
+% 
+T_c_all = zeros(length(vehicle_position_task),M_C+1);
+for ii = 2:M_C+1
     T_c_all(:,ii) = T_c;
 end
-
-
-% The computing time 
+% 
+% % The computing time
 c_i=0.5+rand(length(vehicle_position_task),1);
-c_i_all = ones(length(vehicle_position_task),M+1);
-for ii = 1:M+1
+c_i_all = ones(length(vehicle_position_task),M_C+1);
+for ii = 1:M_C+1
     c_i_all(:,ii) = c_i;
 end  
 T_max = (8+2*rand(length(vehicle_position_task),1));
+f_ij_all= 15/4*ones(length(vehicle_position_task),M_C);
+
+T_v_all=zeros(length(vehicle_position_task),M_C+1);
+T_v_all(:,1) = c_i_all(:,1) / c_car;
+T_v_all(:,2:end) = c_i_all(:,2:end) ./ f_ij_all;
+
+Time_delay= repmat(Time_delay,length(vehicle_position_task),1);
+
+
+
 
 cvx_begin
     cvx_solver MOSEK
-    variable f_ij_all(length(vehicle_position_task),M);
-    variable j_selection(length(vehicle_position_task),M+1) binary;
+    variable j_selection(length(vehicle_position_task),M_C+1) binary;
     expression TT;
-    
-    
-   TT=(j_selection.*(T_d_all+T_c_all));
+%     TT=(j_selection.*(T_d_all+T_c_all+Time_delay));
+%     TT(:,1)=TT(:,1)+j_selection(:,1).*(c_i_all(:,1) / c_car);
+%     
+%     for ii=1:length(vehicle_position_task)
+%         for jj=1:M
+%             TT(ii,jj+1)=TT(ii,jj+1)+ c_i_all(ii,jj+1)*quad_over_lin(j_selection(ii,jj+1),f_ij_all(ii,jj));
+%         end
+%     end
+    TT=j_selection.*(T_d_all+T_c_all+Time_delay+T_v_all);
     TT(:,1)=TT(:,1)+j_selection(:,1).*(c_i_all(:,1) / c_car);
-
-    for ii=1:length(vehicle_position_task)
-        for jj=1:M
-           TT(ii,jj+1)=TT(ii,jj+1)+ c_i_all(ii,jj+1)*quad_over_lin(j_selection(ii,jj+1),f_ij_all(ii,jj));
-        end
-    end
-    
     minimize sum(sum(TT));
-
-subject to 
-    
+        
     for ii = 1:length(vehicle_position_task)
         sum(j_selection(ii,:))==1;          %condition (8c)
     end
     
-    sum(f_ij_all) <= F_limit;                 %condition (8d)
+    sum(j_selection(:,2:end).*f_ij_all) <= F_limit_core;                 %condition (8d)
     
-    for ii=1:length(vehicle_position_task)
-        for jj=1:M
-            f_ij_all(ii,jj)<=F_limit(jj)*j_selection(ii,jj+1);         %condition (8e)
-            f_ij_all(ii,jj)>=0;
-        end
+    sum(TT,2)<= T_max;  %condition (9b)
+    
+    sum(j_selection(:,2:end))<=1;
+    
+    
+    for ii= 1:length(vehicle_position_task)
+         for jj= 1:M_C
+            if L_all_core(ii,jj)<0
+                j_selection(ii,jj+1)==0;
+            end
+         end
     end
-
-      sum(TT,2)<= T_max;                    %condition (9b)
     
-      sum(j_selection(:,2:end))<=buffer_number;        % each server can not connect more than 4 cars(buffer)
-      
       for ii= 1:length(vehicle_position_task)
-          for jj= 1:M
-              if L_all(ii,jj)<0
-                  j_selection(ii,jj+1)==0;
-              end
+        for jj= 1:M_C
+          if L_all_core(ii,jj)> 4*L_m
+              j_selection(ii,jj+1)==0;
           end
-      end
-      
-      for ii= 1:length(vehicle_position_task)
-          for jj= 1:M
-              if L_all(ii,jj)> 4*L_m
-                  j_selection(ii,jj+1)==0;
-              end
-          end
+        end
       end
     
 cvx_end
- 
-    
 
 
-
-% j_selection_matrix = int64(full(j_selection));
-% f_ij_all_matrix=full(f_ij_all);
-% 
-% for ii= 1:length(vehicle_position_task)
-%     for jj =1:M
-%         if f_ij_all_matrix (ii,jj)< 0.001
-%             f_ij_all_matrix(ii,jj) = 0 ;
-%         end
-%     end
-% end  
-
-result_opti=cvx_optval;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+result_opti_core=cvx_optval;  
 
 end
-
--- JOSC_time/P_T_B_L/main.m
+++ JOSC_time/P_T_B_L/main.m
@@ -3,7 +3,7 @@ clc;clear ;
 Z=4;
 M=7*Z;                          % The number of server
 L_m=20;                         % The segment length
-
+M_C=4*M;                      % The number of cores       
  
 v_speed=120*(1000/3600);        % speed of cars (m/s)
 B_RSU=1.25*1000 ;               % Bandwith of the each RSU (HZ)
@@ -11,15 +11,16 @@ P_v=100*(10^(-3));              % the transmission power of each vehicle(watt)
 N_v=10^(-13);                   % the noise power(watt) 
 
 c_car = 0.4;
-F_limit=15*ones(1,M);
+% F_limit=15*ones(1,M);
+F_limit_core=15/4*ones(1,M_C);
 buffer_number=4;
-recy_time=3;
+recy_time=0.5;
 Z_L_m=Z*L_m;
 
-%initial time 
+%initial condition
 N_initial_right=randi([10,25]);
 N_initial_left=randi([10,25]);
-V_postion_x_right_initial=6*Z_L_m.*rand(N_initial_right,1);
+V_postion_x_right_initial=7*Z_L_m.*rand(N_initial_right,1);
 V_postion_y_right_initial =rand(N_initial_right,1);
 for ii=1:N_initial_right
     if V_postion_y_right_initial(ii) >=0.5
@@ -30,7 +31,7 @@ for ii=1:N_initial_right
 end
 
 
-V_postion_x_left_initial=Z_L_m +6*Z_L_m.*rand(N_initial_left,1);
+V_postion_x_left_initial=7*Z_L_m.*rand(N_initial_left,1);
 V_postion_y_left_initial =rand(N_initial_left,1);
 for ii=1:N_initial_left
     if V_postion_y_left_initial(ii) >=0.5
@@ -42,44 +43,84 @@ end
 
 
 
-N_right_add=poissrnd(10);
-N_left_add=poissrnd(10);
-take_average= 200;
-mean_number=15;
-result_opti=zeros(mean_number,take_average);
-count_mean=zeros(mean_number,1);
 
-for ii= 1:mean_number
+% take_average= 200;
+% mean_number=15;
+% % result_opti=zeros(mean_number,take_average);
+% count_mean=zeros(mean_number,1);
+
+% for ii= 1:mean_number
+%     for t_a=1:take_average
+%         N_right_add=poissrnd(ii+10);
+%         N_left_add=poissrnd(ii+10);
+%         count_mean(ii)=ii+10;
+%         [result_opti(ii,t_a),vehicle_position_delete_right,vehicle_position_delete_left] =add_car_opti(N_right_add,N_left_add,L_m,v_speed,recy_time,V_postion_x_right_initial,V_postion_y_right_initial,V_postion_x_left_initial,V_postion_y_left_initial,Z_L_m,B_RSU,M,P_v,N_v,c_car,F_limit,buffer_number);
+%         V_postion_x_right_initial=vehicle_position_delete_right(:,1);
+%         V_postion_y_right_initial=vehicle_position_delete_right(:,2);
+%         V_postion_x_left_initial =vehicle_position_delete_left(:,1);
+%         V_postion_y_left_initial=vehicle_position_delete_left(:,2);
+% 
+%     end
+% end
+% 
+% 
+% result_opti_sum= sum(result_opti,2);
+% result_opti_sum_average= result_opti_sum/take_average;
+% sort_result_opti=sort(result_opti,2);
+% 
+% result_opti_max=sort_result_opti(:,191:end);
+% result_opti_max_average=sum(result_opti_max,2)/10;
+% 
+% result_opti_min=sort_result_opti(:,1:10);
+% result_opti_min_average= sum(result_opti_min,2)/10; 
+% 
+% figure
+% plot(count_mean,result_opti_sum_average);
+% hold on 
+% plot(count_mean,result_opti_max_average);
+% hold on 
+% plot(count_mean,result_opti_min_average);
+% 
+% save('result_opti.mat','result_opti','result_opti_sum_average','result_opti_max_average','result_opti_min_average','result_opti_max','result_opti_min');
+
+
+take_average= 300;
+mean_number=15;
+result_opti_core=zeros(mean_number,take_average);
+count_mean=zeros(mean_number,1);
+Time_delay=zeros(1,M_C+1);
+
+
+
+for ii=1:mean_number
     for t_a=1:take_average
         N_right_add=poissrnd(ii+10);
         N_left_add=poissrnd(ii+10);
         count_mean(ii)=ii+10;
-        [result_opti(ii,t_a),vehicle_position_delete_right,vehicle_position_delete_left] =add_car_opti(N_right_add,N_left_add,L_m,v_speed,recy_time,V_postion_x_right_initial,V_postion_y_right_initial,V_postion_x_left_initial,V_postion_y_left_initial,Z_L_m,B_RSU,M,P_v,N_v,c_car,F_limit,buffer_number);
+        [result_opti_core(ii,t_a),TT,vehicle_position_delete_right,vehicle_position_delete_left,j_selection,vehicle_position_task]=choose_core(F_limit_core,N_right_add,N_left_add,M,L_m,v_speed,B_RSU,P_v,N_v,c_car,recy_time,Z_L_m,Time_delay,V_postion_x_right_initial,V_postion_y_right_initial,V_postion_x_left_initial,V_postion_y_left_initial);
+        TT_matrix=full(TT);
+        
+        for i= 1:length(vehicle_position_task)
+            for jj =1:M_C+1
+                if TT_matrix (i,jj)< 0.001
+                TT_matrix(i,jj) = 0 ;
+                end
+            end
+        end  
+        Time_delay=sum(TT_matrix)-recy_time;
+
+        for jj=1:M_C+1
+            if Time_delay(:,jj)<0
+                Time_delay(:,jj)=0;
+            else
+                Time_delay(:,jj)= Time_delay(:,jj);
+            end
+        end
+        
         V_postion_x_right_initial=vehicle_position_delete_right(:,1);
         V_postion_y_right_initial=vehicle_position_delete_right(:,2);
         V_postion_x_left_initial =vehicle_position_delete_left(:,1);
         V_postion_y_left_initial=vehicle_position_delete_left(:,2);
-
+        
     end
 end
-
-
-result_opti_sum= sum(result_opti,2);
-result_opti_sum_average= result_opti_sum/take_average;
-sort_result_opti=sort(result_opti,2);
-
-result_opti_max=sort_result_opti(:,191:end);
-result_opti_max_average=sum(result_opti_max,2)/10;
-
-result_opti_min=sort_result_opti(:,1:10);
-result_opti_min_average= sum(result_opti_min,2)/10; 
-
-figure
-plot(count_mean,result_opti_sum_average);
-hold on 
-plot(count_mean,result_opti_max_average);
-hold on 
-plot(count_mean,result_opti_min_average);
-
-save('result_opti.mat','result_opti','result_opti_sum_average','result_opti_max_average','result_opti_min_average','result_opti_max','result_opti_min');
-
